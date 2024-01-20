1f64a5ccab8eb3652f1f599a43a3ecc5e2809ae4
DariaOgd <daria.ogd@gmail.com>
2024-01-20 23:26:03 +0100

    up

--- MVCBooksWebApi/MVCBooksWebApi/Models/AddBookViewModel.cs
+++ MVCBooksWebApi/MVCBooksWebApi/Models/AddBookViewModel.cs
@@ -1,12 +1,26 @@
-﻿namespace MVCBooksWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCBooksWebApi.Models
 {
     public class AddBookViewModel
     {
+        [Required(ErrorMessage = "Pole Tytuł jest wymagane.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Pole Autor jest wymagane.")]
         public string Author { get; set; }
+
+        [Required(ErrorMessage = "Pole ISBN jest wymagane.")]
         public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "Pole Ilość stron jest wymagane.")]
         public int Pages { get; set; }
+
+        [Required(ErrorMessage = "Pole Kategoria jest wymagane.")]
         public string Category { get; set; }
+
+        [Required(ErrorMessage = "Pole Ilość jest wymagane.")]
         public int Amount { get; set; }
     }
+
 }
